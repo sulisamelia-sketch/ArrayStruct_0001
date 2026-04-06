058c4d857a9ce03962bb116c724786895c6960b9
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-04-06 11:43:20 +0700

    Menampilkan data mahasiswa

--- struct4.cpp
+++ struct4.cpp
@@ -34,7 +34,18 @@ int main ()
     cin >> mhs[1].umur;
     cin.ignore();
 
+    for (int i = 0; i <= 2; i++)
+    {
+        cout << "Data Mahasiswa Ke-" << i + 1 << " : " << endl;
+        cout << "Nama : " << mhs[i].nama << endl;
+        cout << "Kota : " << mhs[i].alamat.kota << endl;
+        cout << "Nama : " << mhs[i].alamat.desa << endl;
+        cout << "Nama : " << mhs[i].umur << endl;
+        
+
+    }
     
+    }
 
     
 }
