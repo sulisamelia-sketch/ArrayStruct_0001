3f08b4e157b81989a87c7d06aa2d0907b5bdd960
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-04-06 11:21:46 +0700

    Membuat struct alamatDetail

--- struct2.cpp
+++ struct2.cpp
@@ -1,3 +1,37 @@
 #include <iostream>
 using namespace std;
 
+struct alamatDetail
+{
+    string desa;
+    string kota;
+
+};
+
+struct mahasiswa 
+{
+    string nama;
+    alamatDetail alamat;
+    int umur;
+};
+
+int main ()
+{
+    //Deklarasi variabel struct
+    mahasiswa mhs;
+    //Mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Isikan data kota : ";
+    cin >> mhs.alamat.desa;
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur;
+
+    cout << "Data Mahasiswa" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Kota : " << mhs.alamat.kota << endl;
+    cout << "Desa : " << mhs.alamat.desa << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
--- struct4.cpp
+++ struct4.cpp
@@ -9,32 +9,3 @@ struct alamatDetail
 
 };
 
-struct mahasiswa 
-{
-    string nama;
-    alamatDetail alamat;
-    int umur;
-};
-
-int main ()
-{
-    //Deklarasi variabel struct
-    mahasiswa mhs[3];
-
-    //Mengisi data
-    for (int i = 0; i <= 2; i++)
-    {
-    cout << "Isikan data nama : ";
-    getline (cin, mhs[1].nama);
-    cout << "Isikan data kota : ";
-    getline (cin, mhs[1].alamat.kota);
-    cout << "Isikan data desa : ";
-    getline (cin, mhs[1].alamat.desa);
-    cout << "Isikan data umur : ";
-    cin >> mhs[1].umur;
-    cin.ignore();
-    
-    }
-
-    
-}
