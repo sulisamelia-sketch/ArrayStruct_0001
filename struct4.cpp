5dd9dbba6c6eefbc04e3f6537dbda6065acf85f2
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-04-06 11:42:30 +0700

    Input data mahasiswa dengan perulangan

--- struct4.cpp
+++ struct4.cpp
@@ -16,3 +16,26 @@ struct mahasiswa
     int umur;
 };
 
+int main ()
+{
+    //Deklarasi variabel struct
+    mahasiswa mhs[3];
+
+    //Mengisi data
+    for (int i = 0; i <= 2; i++)
+    {
+    cout << "Isikan data nama : ";
+    getline (cin, mhs[1].nama);
+    cout << "Isikan data kota : ";
+    getline (cin, mhs[1].alamat.kota);
+    cout << "Isikan data desa : ";
+    getline (cin, mhs[1].alamat.desa);
+    cout << "Isikan data umur : ";
+    cin >> mhs[1].umur;
+    cin.ignore();
+
+    
+
+    
+}
+
