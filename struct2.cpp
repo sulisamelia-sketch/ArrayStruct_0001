4da592bd3baa5cfa9af1e7be73952c5877bdd1a1
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-04-06 10:57:54 +0700

    Membuat fungsi main dan deklarasi array

---
+++ struct2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa
+{
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main()
+{
+    //Deklarasi variabel struct
+    mahasiswa mhs;
+    //Mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamt : ";
+    cin >> mhs.alamat;
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur;
+
+    cout << "Data Mahasiswa" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
